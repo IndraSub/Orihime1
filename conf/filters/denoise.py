c740436562cf145a71ec1296ea15936d9e2c6a3d
Jianfeng Zhang <swordfeng123@gmail.com>
2018-04-20 01:20:02 +0800

    supporting multi-threading for waifu2x-caffe

--- conf/filters/denoise.py
+++ conf/filters/denoise.py
@@ -65,20 +65,33 @@ def BM3D(core, clip, strength, radius, profile):
     return rep16
 
 
-def Waifu2xCaffe(core, clip, noise, block, model, cudnn, processor, tta):
+def Waifu2xCaffe(core, clip, noise, block, model, cudnn, processor, tta, multi_threads):
     clip = core.fmtc.resample(clip, css="444", csp=vs.YUV444PS)
     clip = core.fmtc.matrix(clip, mat="709", mats="709", matd="709", fulls=True, bits=32)
     rgbs = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=1, matrix_in_s="709", range_in_s="full", filter_param_a=0, filter_param_b=0.5)
-    exp = core.caffe.Waifu2x(
-        rgbs,
-        noise=noise,
-        scale=1,
-        block_w=block,
-        block_h=block,
-        model=model,
-        cudnn=cudnn,
-        processor=processor,
-        tta=tta, )
+    def create_filter(processor):
+        return core.caffe.Waifu2x(
+            rgbs,
+            noise=noise,
+            scale=1,
+            block_w=block,
+            block_h=block,
+            model=model,
+            cudnn=cudnn,
+            processor=processor,
+            tta=tta, )
+    if not multi_threads:
+        exp = create_filter(processor)
+    else:
+        threads_per_device = multi_threads['threads_per_device']
+        devices = multi_threads['devices']
+        threads = threads_per_device * devices
+        exps = [
+            create_filter(p)
+            for i in range(0, threads_per_device)
+            for p in range(0, devices)
+        ]
+        exp = core.std.FrameEval(exps[0], lambda n: exps[n % threads])
     yuv = mvf.ToYUV(exp, css="444", full=False)
     yuv = mvf.Depth(yuv, depth=16, fulls=True, fulld=True, dither=3)
     dei16 = core.resize.Bicubic(clip, format=vs.YUV444P16, matrix_s="709", range_in_s="full", range_s="full", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")

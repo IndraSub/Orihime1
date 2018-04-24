94b267d32eace2b97d2c17e030a7492a141e46cc
Jianfeng Zhang <swordfeng123@gmail.com>
2018-04-24 19:50:43 +0800

    waifu2xcaffe: add clip as param of filter creation waifu2x: try using selectevery/interleave instead of frameeval

--- conf/filters/denoise.py
+++ conf/filters/denoise.py
@@ -69,9 +69,9 @@ def Waifu2xCaffe(core, clip, noise, block, model, cudnn, processor, tta, multi_t
     clip = core.fmtc.resample(clip, css="444", csp=vs.YUV444PS)
     clip = core.fmtc.matrix(clip, mat="709", mats="709", matd="709", fulls=True, bits=32)
     rgbs = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=1, matrix_in_s="709", range_in_s="full", filter_param_a=0, filter_param_b=0.5)
-    def create_filter(processor):
+    def create_filter(clip, processor):
         return core.caffe.Waifu2x(
-            rgbs,
+            clip,
             noise=noise,
             scale=1,
             block_w=block,
@@ -81,17 +81,16 @@ def Waifu2xCaffe(core, clip, noise, block, model, cudnn, processor, tta, multi_t
             processor=processor,
             tta=tta, )
     if not multi_threads:
-        exp = create_filter(processor)
+        exp = create_filter(rgbs, processor)
     else:
         threads_per_device = multi_threads['threads_per_device']
         devices = multi_threads['devices']
         threads = threads_per_device * devices
-        exps = [
-            create_filter(p)
+        exp = core.std.Interleave([
+            create_filter(rgbs.std.SelectEvery(cycle=threads, offsets=i * devices + p), p)
             for i in range(0, threads_per_device)
             for p in range(0, devices)
-        ]
-        exp = core.std.FrameEval(exps[0], lambda n: exps[n % threads])
+        ])
     yuv = mvf.ToYUV(exp, css="444", full=False)
     yuv = mvf.Depth(yuv, depth=16, fulls=True, fulld=True, dither=3)
     dei16 = core.resize.Bicubic(clip, format=vs.YUV444P16, matrix_s="709", range_in_s="full", range_s="full", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")

94054b9737364b54ce3fb07f48fc9f22686492c4
Jianfeng Zhang <swordfeng123@gmail.com>
2018-04-25 23:35:33 +0800

    add a final "press any key to continue"

--- media.sh
+++ media.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 export WINEDEBUG=-all # disable wine debug output
 echo -ne '\033]2;INDRA Rip Tools\007'
 script=$(readlink -f "$0")
 rootdir=$(dirname "$script")
 cd "$rootdir"
 python3 -m libraries.tree_diagram
+read -rsn1 -p "Press any key to continue"; echo

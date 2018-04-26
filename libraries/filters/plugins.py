6acc390375057c6a70c7d48e68915ac8d70fc3d9
Jianfeng Zhang <swordfeng123@gmail.com>
2018-04-27 00:06:31 +0800

    fix variable name

--- libraries/filters/plugins.py
+++ libraries/filters/plugins.py
@@ -10,12 +10,12 @@ info = json.loads(os.environ['TDINFO'])
 
 def load_plugins(core):
     path = join(dirname(__file__), '..', '..', 'bin', platform.system().lower(), 'filter_plugins')
-    for name in info['vsplugins']:
+    for name in info['vsfilters']:
         print('[DEBUG][Plugin Loader] Loading VapourSynth plugin: '+name, file=sys.stderr)
         try:
             core.std.LoadPlugin(join(path, 'vs', name))
         except vs.Error as e:
             print(f'[DEBUG][Plugin Loader] Load {name} failed with error: '+str(e), file=sys.stderr)
-    for name in info['avsplugins']:
+    for name in info['avsfilters']:
         print('[DEBUG][Plugin Loader] Loading AviSynth plugin: '+name, file=sys.stderr)
         core.avs.LoadPlugin(join(path, 'avs', name))

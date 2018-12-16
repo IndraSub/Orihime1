1bd61a4b9c52c423c1f81d716dd7cc6474601f63
TundraWork <tundrawork@gmail.com>
2018-12-17 00:36:24 +0800

    filters: update new methods

--- libraries/filters/__init__.py
+++ libraries/filters/__init__.py
@@ -1,6 +1,7 @@
 from .crop_after import CropAfter
 from .crop_before import CropBefore
 from .deband import f3kdb
+from .deinterlace import IT, VIVTC, TIVTC, Yadifmod, QTGMC
 from .delogo import Delogo
 from .denoise import SMDegrain, SMDegrainFast, BM3D, Waifu2xCaffe, Waifu2xW2XC, VagueDenoiser, MisakaDenoise
 from .dering import Dering
@@ -10,8 +11,6 @@ from .line_clearness import LineClearness
 from .line_sharp import LineSharp
 from .format import Format
 from .mcfi import SVP
-from .deinterlace import IT, VIVTC, TIVTC, Yadifmod, QTGMC
-from .post_process import CSMOD
 from .resolution import Resolution
 from .source import LWLibavSource, LSMASHVideoSource, FFMS2, AVISource, MultiSource
 from .subtitle import VSFilterMod, Subtext, InfoText

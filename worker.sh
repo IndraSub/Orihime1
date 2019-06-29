d78a1fd1cc949f68dad149fe31fbe1fd14976861
swordfeng <swordfeng123@gmail.com>
2019-06-29 15:56:23 -0600

    Prevent mishandling of spaces

--- worker.sh
+++ worker.sh
@@ -2,8 +2,8 @@
 export WINEDEBUG=-all # disable wine debug output
 export LC_ALL=zh_CN.UTF-8
 echo -ne '\033]2;INDRA Rip Tools\007'
-script=$(readlink -f "$0")
-rootdir=$(dirname "$script")
+script="$(readlink -f "$0")"
+rootdir="$(dirname "$script")"
 cd "$rootdir"
 python3.6 -m libraries.worker
 sleep 1

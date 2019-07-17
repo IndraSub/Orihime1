81e179ef185818387a2468c072e466ab25423689
Tundra Work <tundrawork@gmail.com>
2019-07-17 21:11:28 +0800

    CSMOD: add limitsrc parameter

--- libraries/filters/post_process.py
+++ libraries/filters/post_process.py
@@ -4,6 +4,6 @@ from .utils import ConfigureError, SimpleFilter
 
 @SimpleFilter
 def CSMOD(core, clip, _, _clip, preset="very slow"):
-    return _CSMOD.CSMOD(filtered=clip, source=_clip['source'], preset=preset)
+    return _CSMOD.CSMOD(filtered=clip, source=_clip['source'], preset=preset, limitsrc=True)
 
 

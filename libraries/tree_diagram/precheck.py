39dc622fd6a8e7c0832f6d3efd672aa4c0a3d21d
Tundra Work <tundrawork@gmail.com>
2019-07-17 21:21:09 +0800

    precheck: python 3.7 should be supported

--- libraries/tree_diagram/precheck.py
+++ libraries/tree_diagram/precheck.py
@@ -66,8 +66,8 @@ def checkSystem() -> None:
     logger.info(f'ENCODING: {default_encoding}')
 
     passed = True
-    if sys.version_info < (3, 6) or sys.version_info >= (3, 7):
-        logger.error('Python version should be 3.6')
+    if sys.version_info < (3, 6) or sys.version_info > (3, 7):
+        logger.error('Python version should be 3.6 or 3.7')
         passed = False
     if plat_info.system not in ['Windows', 'Linux']:
         logger.error('Unsupported operating system')

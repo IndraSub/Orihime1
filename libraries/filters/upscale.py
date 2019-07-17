27cfc642fe0d9713f3f5ceef5e018d4a176d9e1d
Tundra Work <tundrawork@gmail.com>
2019-07-17 21:14:34 +0800

    Waifu2xExpand: refactor

--- libraries/filters/upscale.py
+++ libraries/filters/upscale.py
@@ -8,21 +8,33 @@ from .utils import ConfigureError, SimpleFilter
 
 
 @SimpleFilter
-def Waifu2xExpand(core, clip, _, scale, noise, block, model, cudnn, processor, tta, batch):
+def Waifu2xExpand(core, clip, _, scale, noise, block, model, cudnn, processor, tta, batch, multi_threads):
     clip = core.fmtc.resample(clip, css="444", csp=vs.YUV444PS)
     clip = core.fmtc.matrix(clip, mat="709", mats="709", matd="709", fulls=False, bits=32)
     rgbs = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=1, matrix_in_s="709", range_in_s="limited", filter_param_a=0, filter_param_b=0.5)
-    exp = core.caffe.Waifu2x(
-        rgbs,
-        noise=noise,
-        scale=scale,
-        block_w=block,
-        block_h=block,
-        model=model,
-        cudnn=cudnn,
-        processor=processor,
-        tta=tta,
-        batch=batch)
+    def create_filter(clip, processor):
+        return core.caffe.Waifu2x(
+            rgbs,
+            noise=noise,
+            scale=scale,
+            block_w=block,
+            block_h=block,
+            model=model,
+            cudnn=cudnn,
+            processor=processor,
+            tta=tta,
+            batch=batch)
+    if not multi_threads:
+        exp = create_filter(rgbs, processor)
+    else:
+        threads_per_device = multi_threads['threads_per_device']
+        devices = multi_threads['devices']
+        threads = threads_per_device * devices
+        exp = core.std.Interleave([
+            create_filter(rgbs.std.SelectEvery(cycle=threads, offsets=i * devices + p), p)
+            for i in range(0, threads_per_device)
+            for p in range(0, devices)
+        ]) if threads > 1 else create_filter(rgbs, processor)
     yuv = mvf.ToYUV(exp, css="444", full=False)
     yuv = mvf.Depth(yuv, depth=10, fulls=False, fulld=False, dither=3)
     return yuv

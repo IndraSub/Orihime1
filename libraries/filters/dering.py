a8b2ec93f3f51563231fdcc0bd7f275e146e79a0
Tundra Work <tundrawork@gmail.com>
2020-06-22 01:29:43 +0800

    filters: Dering: export threshold parameter

--- libraries/filters/dering.py
+++ libraries/filters/dering.py
@@ -2,15 +2,16 @@ from vapoursynth_tools import havsfunc as haf
 
 
 class Dering:
-    def __init__(self, _):
-        pass
+    def __init__(self, _, threshold=60):
+        self.threshold = threshold
+
     def __call__(self, core, clip):
         return haf.HQDeringmod(
             clip,
             mrad=1,
             msmooth=1,
             incedge=False,
-            mthr=60,
+            mthr=self.threshold,
             minp=1,
             nrmode=2,
             sharp=1,

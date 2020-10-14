690ccd89814ad75dcf2a5b635ccac27755fbfa7d
Tundra Work <tundrawork@gmail.com>
2020-10-14 20:39:17 +0800

    filters/LGhost: introducing LGhost filter, a deringing filter

--- libraries/filters/dering.py
+++ libraries/filters/dering.py
@@ -6,3 +6,8 @@ from .utils import SimpleFilter
 @SimpleFilter
 def HQDeringmod(core, clip, _, threshold=60, inpand=1, edge_expand=1, edge_smooth=1, planes=[0]):
     return haf.HQDeringmod(clip, mrad=edge_expand, msmooth=edge_smooth, mthr=threshold, minp=inpand, planes=planes)
+
+
+@SimpleFilter
+def LGhost(core, clip, _, mode, shift, strength, planes=[0]):
+    return core.lghost.LGhost(clip, mode, shift, strength, planes)

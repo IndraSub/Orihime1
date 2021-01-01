772882bc51784edf0c211311e01ec570ea42f92b
Tundra Work <tundrawork@gmail.com>
2021-01-01 17:48:36 +0800

    filters/denoise: add Waifu2xNCNN filter, other fixes

--- libraries/filters/denoise.py
+++ libraries/filters/denoise.py
@@ -18,7 +18,7 @@ def SMDegrain(core, clip, _):
         thSCD1=1200,
         blksize=16,
         search=5,
-        prefilter=2,
+        prefilter=4,
         contrasharp=True,
         RefineMotion=True,
         pel=2,
@@ -37,7 +37,7 @@ def SMDegrainFast(core, clip, _):
         tr=2,
         blksize=32,
         overlap=16,
-        prefilter=2,
+        prefilter=4,
         contrasharp=30,
         RefineMotion=True,
         pel=1,
@@ -58,6 +58,33 @@ def BM3D(core, clip, _, strength, radius, profile):
         profile1=profile,
         refine=1, )
     return core.resize.Bicubic(den, format=vs.YUV444P16, matrix_s="709", range_in_s="limited", range_s="limited", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")
+
+
+@SimpleFilter
+def Waifu2xNCNN(core, clip, _, noise, block, model, gpu, multi_threads, precision=16):
+    clip = core.fmtc.resample(clip, css="444", csp=vs.YUV444PS, fulls=False)
+    clip = core.fmtc.matrix(clip, mat="709", mats="709", matd="709", fulls=False, bits=32)
+    rgbs = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=1, matrix_in_s="709", range_in_s="limited", filter_param_a=0, filter_param_b=0.5)
+    def create_filter(clip, gpu):
+        return core.w2xnvk.Waifu2x(
+            clip,
+            noise=noise,
+            scale=1,
+            tile_size=block,
+            model=model,
+            precision=precision,
+            gpu_id=gpu,
+            gpu_thread=multi_threads['threads_per_device'])
+    if not multi_threads:
+        multi_threads = {'devices': 1, 'threads_per_device': 1}
+        exp = create_filter(rgbs, gpu)
+    else:
+        if not multi_threads['devices'] == 1:
+            raise ConfigureError('Waifu2xNCNN: multi_threads: devices can not set other than 1')
+        exp = create_filter(rgbs, gpu)
+    yuv = mvf.ToYUV(exp, css="444", full=False)
+    yuv = mvf.Depth(yuv, depth=16, fulls=False, fulld=False, dither=3)
+    return core.resize.Bicubic(yuv, format=vs.YUV444P16, matrix_s="709", range_in_s="limited", range_s="limited", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")
 
 
 @SimpleFilter
@@ -90,10 +117,7 @@ def Waifu2xCaffe(core, clip, _, noise, block_w, block_h, model, cudnn, processor
         ]) if threads > 1 else create_filter(rgbs, processor)
     yuv = mvf.ToYUV(exp, css="444", full=False)
     yuv = mvf.Depth(yuv, depth=16, fulls=False, fulld=False, dither=3)
-    dei16 = core.resize.Bicubic(clip, format=vs.YUV444P16, matrix_s="709", range_in_s="limited", range_s="limited", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")
-    den16 = core.resize.Bicubic(yuv, format=vs.YUV444P16, matrix_s="709", range_in_s="limited", range_s="limited", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")
-    rep16 = core.rgvs.Repair(den16, dei16, mode=1)
-    return rep16
+    return core.resize.Bicubic(yuv, format=vs.YUV444P16, matrix_s="709", range_in_s="limited", range_s="limited", filter_param_a=0, filter_param_b=0.5, dither_type="error_diffusion")
 
 
 @SimpleFilter
